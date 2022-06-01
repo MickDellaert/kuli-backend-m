e1d37690e6bd6e8735ddeb375b7d080c34faaa86
denrique-alvarez <denrique.alvarez@gmail.com>
2022-06-01 16:58:30 +0200

    test daniel

---
+++ src/api/daniel/controllers/daniel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  daniel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::daniel.daniel');
---
+++ src/api/daniel/services/daniel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * daniel service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::daniel.daniel');
