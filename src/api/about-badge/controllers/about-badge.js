28640621c0de5288c8d4f37c7e4bd25974e31d7a
denrique-alvarez <denrique.alvarez@gmail.com>
2022-06-10 14:58:52 +0200

    our system page

---
+++ src/api/about-badge/controllers/about-badge.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  about-badge controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-badge.about-badge');
---
+++ src/api/about-badge/services/about-badge.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-badge service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::about-badge.about-badge');
