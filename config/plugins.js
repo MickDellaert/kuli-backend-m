a37e99f7cf931a49366d6207989bf909f4e000d8
denrique-alvarez <denrique.alvarez@gmail.com>
2022-06-14 00:18:17 +0200

    config

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: 'doz3koti5',
+        api_key: '518415748592659',
+        api_secret: 'NXG3KRY3y97yiDOH_Qyp57NHz2w',
       },
       actionOptions: {
         upload: {},
